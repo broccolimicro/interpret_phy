a39fd9c00ed4154ce6ed109889f36cc7c4ced9cc
Ned Bingham <edward.bingham@broccolimicro.io>
2024-09-16 08:20:59 -0400

    defining pi

--- interpret_phy/export.h
+++ interpret_phy/export.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define _USE_MATH_DEFINES
+// windows doesn't it's constants in math.h, this is required to compile gdstk import on windows.
+#define M_PI 3.14159265358979323846
 #include <gdstk/gdstk.hpp>
 #include <phy/Library.h>
 
